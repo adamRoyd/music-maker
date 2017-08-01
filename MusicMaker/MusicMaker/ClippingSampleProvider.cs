6cd57992cfd1fce4595e3715fa747ef118ece283
Adam Boothroyd BW <adam.boothroyd@brightwavegroup.com>
2017-08-01 13:27:40 +0100

    ClippingSampleProvider class

---
+++ MusicMaker/MusicMaker/ClippingSampleProvider.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NAudio.Wave;
+
+namespace MusicMaker
+{
+    class ClippingSampleProvider : ISampleProvider
+    {
+        private ISampleProvider source;
+
+        public ClippingSampleProvider(ISampleProvider source)
+        {
+            this.source = source;
+        }
+        WaveFormat ISampleProvider.WaveFormat
+        {
+            get { return source.WaveFormat; }
+        }
+
+        int ISampleProvider.Read(float[] buffer, int offset, int count)
+        {
+            int sampleRead = source.Read(buffer, offset, count);
+            for (int n = 0; n < sampleRead; n++)
+            {
+                if (buffer[offset + n] > 1.0f)
+                    buffer[offset + n] = 1.0f;
+                else if (buffer[offset + n] < -1.0f)
+                    buffer[offset + n] = -1.0f;
+            }
+            return sampleRead;
+        }
+    }
+}
